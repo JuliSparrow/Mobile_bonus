13ec57bb18cfa3fa3c72435f54c3197755793144
QA <feverx@inbox.ru>
2023-02-08 19:07:55 +0300

    сделан алгоритм расчета бонусов и баланса

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,23 @@ public class Main {
         // Объявляете переменные для входных данных и
         // параметров программы: начального счёта,
         // суммы пополнения и тп
+        int balance = 900;
+        int amount = 100;
+        int minAmount = 1000;
+        int bonusCost = 100;
+        int bonus;
 
         // Условным оператором проверяете, превысила ли
         // сумма пополнения порог, и для этих двух разных
         // сценариев рассчитываете сумму бонуса и выводите
         // на экран.
+        if (amount > minAmount) {
+            bonus = amount / bonusCost;
+        } else {
+            bonus = 0;
+        }
+        balance = balance + amount + bonus;
+        System.out.println(balance);
+        System.out.println(bonus);
     }
 }
